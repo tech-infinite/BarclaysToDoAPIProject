be6958c14f8355e4d343dc4b206f8382f68cc279
Supraja Ramaraju <test.email@example.com>
2023-09-20 19:12:03 +0100

    Added properties to the TaskItem class.

--- BarclaysToDoApplication/Models/TaskItems.cs
+++ BarclaysToDoApplication/Models/TaskItems.cs
@@ -1,6 +1,18 @@
-﻿namespace BarclaysToDoApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarclaysToDoApplication.Models
 {
     public class TaskItems
     {
+
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Task Name is required.")]
+        [StringLength(100, ErrorMessage = "Task Name cannot exceed 100 characters.")]
+        public string TaskName { get; set; }
+        
+        [Range(1, 5, ErrorMessage = "Priority must be between 1 and 5.")]
+        public int Priority { get; set; }
+        public string Status { get; set; }
     }
 }
