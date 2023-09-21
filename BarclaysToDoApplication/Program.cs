83bb19cf3cdcb260b3a45bb25da08a505227485c
Supraja Ramaraju <test.email@example.com>
2023-09-21 18:06:24 +0100

    Changed the name of API

--- BarclaysToDoApplication/Program.cs
+++ BarclaysToDoApplication/Program.cs
@@ -17,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo Application");
     });
 }
 
